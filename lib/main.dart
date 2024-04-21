b5ebdeeae741055790e865455cbdead3321ffdb6
zhanghaojie <1455123087@qq.com>
2024-04-21 09:40:39 +0800

    Add a autofocus text field and handle user input in MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -55,17 +55,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final textEditingController = TextEditingController();
+  bool _textFieldVisible = false;
 
-  void _incrementCounter() {
+  void _handleAddClick() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _textFieldVisible = true;
     });
+  }
+
+  void _hideTextField() {
+    setState(() {
+      _textFieldVisible = false;
+    });
+  }
+
+  void _handleEditingComplete() {
+    print(textEditingController.text);
+    textEditingController.clear();
+    _hideTextField();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
   }
 
   @override
@@ -87,36 +101,27 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: _textFieldVisible
+                  ? TextField(
+                      autofocus: true,
+                      controller: textEditingController,
+                      onEditingComplete: _handleEditingComplete,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter your username',
+                      ),
+                    )
+                  : null,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _handleAddClick,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
